34b13ee820b35ed70c350988edfa9aaf8b38c8d1
PaLlAbS09 <sarkarpallab471@gmail.com>
2026-02-14 05:44:06 +0530

    add new page

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
+
+document.querySelectorAll('[data-bs-toggle="submenu"]').forEach(btn => {
+  btn.addEventListener('click', function (e) {
+    e.preventDefault();
+    e.stopPropagation();
+
+    const parent = this.closest('.dropdown-submenu');
+
+    parent.classList.toggle('show');
+  });
+});
+
 const btn = document.querySelector(".scroll-top");
 
 window.addEventListener("scroll", () => {
